fc9942ca4dbdd03faff7ec4c0a1b93114c314d7c
Joao <0101lopezru@gmail.com>
2023-04-22 16:53:46 -0500

    valid format email, size and regest

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,16 +1,31 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+//import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Usuario {
-
+	
+	//@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+	private String identificador;
+	
+	//@NotEmpty(message = "el nombre no puede ser vacio")
+	private String nombre;
+	
 	@NotEmpty
+	private String apellido;
+
+	@NotBlank
+	@Size(min = 3, max=8)
 	private String username;
 	
 	@NotEmpty
 	private String password;
 	
 	@NotEmpty
+	@Email(message = "correo con formato incorrecto")
 	private String email;
 
 	public String getUsername() {
@@ -35,6 +50,30 @@ public class Usuario {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
 	}
 
 }
